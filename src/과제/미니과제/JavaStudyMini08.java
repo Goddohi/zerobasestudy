47596d7589362570c51fbe3aa936c8d3a2f85d81
goddohi <kmam147@gmail.com>
2024-07-30 07:09:59 +0900

    임시저장

---
+++ src/과제/미니과제/JavaStudyMini08.java
@@ -0,0 +1,18 @@
+package 과제.미니과제;
+
+import java.util.Scanner;
+
+public class JavaStudyMini08 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("[과세금액 계산 프로그램]\n");
+        System.out.print("연소득을 입력해 주세요.:");
+        long num = scanner.nextLong();
+        long[] gwase = {12000000L, 46000000L, 88000000L,150000000L,300000000L,10000000000L};
+        long result = 0;
+        for(int i=0; i<gwase.length; i++){
+            long temp;
+        }
+        //하는중
+    }
+}
